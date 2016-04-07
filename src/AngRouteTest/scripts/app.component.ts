eacf88bc2825b3e0bfaf37afc984796ffb68a29a
Mark Test <mjfschool@yahoo.com>
2016-04-07 14:54:25 -0400

    intial bookmark layout

--- src/AngRouteTest/scripts/app.component.ts
+++ src/AngRouteTest/scripts/app.component.ts
@@ -1,37 +1,49 @@
 ﻿import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
-import {CrisisCenterComponent} from './crisis-center/crisis-center.component';
-import {HeroListComponent}     from './heroes/hero-list.component';
-import {HeroDetailComponent}   from './heroes/hero-detail.component';
+import {HomeComponent} from './home.component';
+import {SearchComponent} from './search.component';
+import {BookmarkOptionComponent} from './bookmark-option.component';
 
-import {DialogService}         from './dialog.service';
-import {HeroService}           from './heroes/hero.service';
+
+
+//import {CrisisCenterComponent} from './crisis-center/crisis-center.component';
+//import {HeroListComponent}     from './heroes/hero-list.component';
+//import {HeroDetailComponent}   from './heroes/hero-detail.component';
+
+//import {DialogService}         from './dialog.service';
+//import {HeroService}           from './heroes/hero.service';
 
 @Component({
     selector: 'my-app',
     template: `
     <h1 class="title">Component Router</h1>
     <nav>
-      <a [routerLink]="['CrisisCenter']">Crisis Center</a>
-      <a [routerLink]="['Heroes']">Heroes</a>
+      <a [routerLink]="['Home']">Home</a>
+      <a [routerLink]="['Search']">Search</a>
+      <a [routerLink]="['Option']">Bookmark Option</a>
     </nav>
     <router-outlet></router-outlet>
   `,
-    providers: [DialogService, HeroService],
+    //providers: [DialogService, HeroService],
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
 
-    { // Crisis Center child route
-        path: '/crisis-center/...',
-        name: 'CrisisCenter',
-        component: CrisisCenterComponent,
-        useAsDefault: true
-    },
+    //{ // Crisis Center child route
+    //    path: '/crisis-center/...',
+    //    name: 'CrisisCenter',
+    //    component: CrisisCenterComponent,
+    //    useAsDefault: true
+    //},
 
-    { path: '/heroes', name: 'Heroes', component: HeroListComponent },
-    { path: '/hero/:id', name: 'HeroDetail', component: HeroDetailComponent },
-    { path: '/disaster', name: 'Asteroid', redirectTo: ['CrisisCenter', 'CrisisDetail', { id: 3 }] }
+    //{ path: '/heroes', name: 'Heroes', component: HeroListComponent },
+    //{ path: '/hero/:id', name: 'HeroDetail', component: HeroDetailComponent },
+    //{ path: '/disaster', name: 'Asteroid', redirectTo: ['CrisisCenter', 'CrisisDetail', { id: 3 }] }
+
+
+    { path: '/home', name: 'Home', component: HomeComponent },
+    { path: '/search', name: 'Search', component: SearchComponent },
+    { path: '/detail', name: 'Option', component: BookmarkOptionComponent }
 ])
 export class AppComponent { }
---
+++ src/AngRouteTest/scripts/bookmark-option.component.ts
@@ -0,0 +1,19 @@
+﻿import {Component}   from 'angular2/core';
+import {Router, RouteParams} from 'angular2/router';
+
+
+@Component({
+    template: `
+    <h2>Boomark Option</h2>
+  `
+})
+
+
+export class BookmarkOptionComponent {
+
+
+
+
+
+
+}
