3c964c2dfc2b3392ea0e41fd957f0d8cceaf8fbe
Mark Test <mjfschool@yahoo.com>
2016-04-07 16:46:09 -0400

    nav url working

--- src/AngRouteTest/Startup.cs
+++ src/AngRouteTest/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 namespace AngRouteTest
 {
@@ -25,9 +26,32 @@ namespace AngRouteTest
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            //app.Run(async (context) =>
+            // Route all unknown requests to app root
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                // If there's no available file and the request doesn't contain an extension, we're probably trying to access a page.
+                // Rewrite request to use app root
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = "/index.html"; // Put your Angular root page here 
+                    await next();
+                }
+            });
+
+            // Serve wwwroot as root
+            app.UseFileServer();
+
+            // Serve /node_modules as a separate root (for packages that use other npm modules client side)
+            //app.UseFileServer(new FileServerOptions()
             //{
-            //    await context.Response.WriteAsync("Hello World!");
+            //    // Set root of file server
+            //    FileProvider = new PhysicalFileProvider(Path.Combine(environment.ApplicationBasePath, "node_modules")),
+            //    // Only react to requests that match this path
+            //    RequestPath = "/node_modules",
+            //    // Don't expose file system
+            //    EnableDirectoryBrowsing = false
             //});
         }
 
